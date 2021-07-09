3157aebc9dd14d50f4c84b21e6a2f29688eb96da
Green <green.onyeji@gmail.com>
2021-07-09 06:06:36 +0100

    edited the about component

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -25,12 +25,12 @@ export default class Sidebar extends Component {
             </nav>
             <nav id="colorlib-main-menu">
               <ul>
-                <li><a href="#" target="_blank" rel="noopener noreferrer"><i className="icon-facebook2" /></a></li>
-                <li><a href="#" target="_blank" rel="noopener noreferrer"><i className="icon-twitter2" /></a></li>
-                <li><a href="#" target="_blank" rel="noopener noreferrer"><i className="icon-instagram" /></a></li>
-                <li><a href="#" target="_blank" rel="noopener noreferrer"><i className="icon-linkedin2" /></a></li>
-                <li><a href="#" target="_blank" rel="noopener noreferrer"><i className="icon-github"></i></a></li>
-                <li><a href="#" target="_blank" rel="noopener noreferrer"><i className="icon-blogger2"></i></a></li>
+                <li><a href="https://github.com/ositano" target="_blank" rel="noopener noreferrer"><i className="icon-facebook2" /></a></li>
+                <li><a href="https://github.com/ositano" target="_blank" rel="noopener noreferrer"><i className="icon-twitter2" /></a></li>
+                <li><a href="https://github.com/ositano" target="_blank" rel="noopener noreferrer"><i className="icon-instagram" /></a></li>
+                <li><a href="https://www.linkedin.com/in/green-onyeji-11608a97/" target="_blank" rel="noopener noreferrer"><i className="icon-linkedin2" /></a></li>
+                <li><a href="https://github.com/ositano" target="_blank" rel="noopener noreferrer"><i className="icon-github"></i></a></li>
+                
               </ul>
             </nav>
             <div className="colorlib-footer">
